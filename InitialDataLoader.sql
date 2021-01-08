2e50b6843166b5b971e2b232d1c056c8b62f8d6a
sebrown0 <thebrownfamily@live.co.uk>
2021-01-08 18:15:18 +0100

    Create procedure to check test results. Move all data into data dir

--- InitialDataLoader.sql
+++ InitialDataLoader.sql
@@ -2,7 +2,7 @@
 
 TEMPLATE
 DELETE FROM XXXX WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\XXXX.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\XXXX.csv'
 INTO TABLE employee_aeo 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -13,104 +13,104 @@ TEMPLATE */
 USE salaroo_uk;
 SET foreign_key_checks = 0;
 
-DELETE FROM tax_year WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\tax_year.csv'
+TRUNCATE tax_year;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\tax_year.csv'
 INTO TABLE tax_year 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM student_loan WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\student_loan.csv'
+TRUNCATE student_loan;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\student_loan.csv'
 INTO TABLE student_loan 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM leave_type WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\leave_type.csv'
+TRUNCATE leave_type;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\leave_type.csv'
 INTO TABLE leave_type 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM leave_booking WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\leave_booking.csv'
+TRUNCATE leave_booking;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\leave_booking.csv'
 INTO TABLE leave_booking 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM employment_type WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\employment_type.csv'
+TRUNCATE employment_type;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\employment_type.csv'
 INTO TABLE employment_type 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM employer WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\employer.csv'
+TRUNCATE employer;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\employer.csv'
 INTO TABLE employer 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM employee_payroll_details WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\employee_payroll_details.csv'
+TRUNCATE employee_payroll_details;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\employee_payroll_details.csv'
 INTO TABLE employee_payroll_details 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM aeo_type WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\aeo_type.csv'
+TRUNCATE aeo_type;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\aeo_type.csv'
 INTO TABLE aeo_type 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM aeo_agency WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\aeo_agency.csv'
+TRUNCATE aeo_agency;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\aeo_agency.csv'
 INTO TABLE aeo_agency 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM address WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\emp_address.csv'
+TRUNCATE address;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\emp_address.csv'
 INTO TABLE address 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM employee WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\emp.csv'
+TRUNCATE employee;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\emp.csv'
 INTO TABLE employee 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM employee_aeo WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\emp_aeo_data.csv'
+TRUNCATE employee_aeo;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\emp_aeo_data.csv'
 INTO TABLE employee_aeo 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DELETE FROM payroll_frequency WHERE id <> '';
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\payroll_frequency.csv'
+TRUNCATE payroll_frequency;
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\initial_data\\payroll_frequency.csv'
 INTO TABLE payroll_frequency 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
--- LoadTestData.sql
+++ LoadTestData.sql
@@ -4,7 +4,7 @@ USE salaroo_uk;
 SET foreign_key_checks = 0;
 
 TRUNCATE test_data_salary;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_data_salary.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_data_salary.csv'
 INTO TABLE test_data_salary 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -13,7 +13,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test_data_aeo;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_data_aeo.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_data_aeo.csv'
 INTO TABLE test_data_aeo 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -22,7 +22,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test_data_student_loan;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_data_student_loan.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_data_student_loan.csv'
 INTO TABLE test_data_student_loan 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -31,7 +31,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test_data_ytd;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_data_ytd.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_data_ytd.csv'
 INTO TABLE test_data_ytd 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -40,7 +40,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test_result_expected;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_result_expected.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_result_expected.csv'
 INTO TABLE test_result_expected 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -49,7 +49,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test_group;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_group.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_group.csv'
 INTO TABLE test_group 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -58,7 +58,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test_data_employee;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_data_employee.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_data_employee.csv'
 INTO TABLE test_data_employee 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -67,7 +67,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test_data_payroll_params;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test_data_payroll_params.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test_data_payroll_params.csv'
 INTO TABLE test_data_payroll_params 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
@@ -76,7 +76,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 TRUNCATE test;
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\test_data\\test.csv'
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\salaroo_uk\\data\\test_data\\test.csv'
 INTO TABLE test 
 CHARACTER SET latin1
 FIELDS TERMINATED BY ',' 
