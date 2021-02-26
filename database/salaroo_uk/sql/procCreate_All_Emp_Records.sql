26d98c825ebc41aca081047cb9417afa699181f2
sebrown0 <thebrownfamily@live.co.uk>
2021-02-26 17:42:08 +0100

    Split eligibility into its proper components

--- database/salaroo_uk/sql/procCreate_All_Emp_Records.sql
+++ database/salaroo_uk/sql/procCreate_All_Emp_Records.sql
@@ -13,7 +13,8 @@ BEGIN
 		epd.commision_percentage, epd.gets_blind_allowance, epd.nic, epd.pay_frequency, epd.avg_hours_per_week,  
 		emp.first_name, emp.last_name, emp.date_of_birth, emp.gender, emp.employer_id,
 		hr.ni_number, hr.start_date ,hr.martial_status , hr.employee_hours_normally_worked,
-		et.employee_type, et.primary_employment AS is_primary_employment, et.is_apprentice, et.is_casual_worker
+		et.employee_type, et.primary_employment AS is_primary_employment, et.is_apprentice, et.is_casual_worker,
+        get_employees_age(epd.employee_id) AS emp_age
 	FROM employee_hr hr 
 		JOIN employee_payroll_details epd ON hr.ni_number = epd.payroll_id
 			JOIN employee emp ON epd.employee_id = emp.id
